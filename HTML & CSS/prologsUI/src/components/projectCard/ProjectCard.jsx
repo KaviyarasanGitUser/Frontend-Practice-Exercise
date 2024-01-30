8e6db363e42050b79bf335ead7bda470065d81da
Kaviyarasan <duraisamy.kaviyarasan@divum.in>
2024-01-30 17:43:01 +0530

    last commit

--- HTML & CSS/prologsUI/src/components/projectCard/ProjectCard.jsx
+++ HTML & CSS/prologsUI/src/components/projectCard/ProjectCard.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import "./ProjectCard.css"
+import React from "react";
+import "./ProjectCard.css";
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
   return (
-    <div className='project-menu'>
-        <p className='icon' style={{backgroundColor:project.background}}>{project.icon}</p>
-        <p className="project-status">{project.projectStatus}</p>
-        <p className='project-count'>{project.numberOfProject}</p>
+    <div className="project-menu">
+      <p className="icon" style={{ backgroundColor: project.background }}>
+        {project.icon}
+      </p>
+      <p className="project-status">{project.projectStatus}</p>
+      <p className="project-count">{project.numberOfProject}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
---
+++ TypeScript/remainders-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div><button className="btn btn-primary">Click me</button></div>
+  );
+}
+
+export default App;
